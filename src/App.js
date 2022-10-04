8d772ddfbb8b7227ac3aa91de4ddc177d5ff7f54
Rocio <rociocelasco.r@gmail.com>
2022-10-04 22:50:31 +0200

    crear formulario y llamada a la api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import "./App.css";
+import { useState } from "react";
+import formatDate from "./formatDate";
+
+const today = formatDate(new Date());
+
 function App() {
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(`https://apidatos.ree.es/es/datos/balance/balance-electrico?start_date=${startDate}T00:00&end_date=${endDate}T23:59&time_trunc=day`)
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data.included);
+      });
+  }
+
   return (
-    <div>
-      Hello World!
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <label>Fecha de Inicio
+          <input
+            type="date"
+            name="startDate"
+            max={today}
+            onChange={(event) => setStartDate(event.target.value)} />
+        </label>
+
+        <label>Fecha Final
+          <input
+            type="date"
+            name="endDate"
+            min={startDate}
+            max={today}
+            onChange={(event) => setEndDate(event.target.value)} />
+        </label>
+
+        <button type="submit">
+          Generar Gráfica
+        </button>
+      </form>
     </div>
   );
 }
---
+++ src/formatDate.js
@@ -0,0 +1,17 @@
+const formatDate = (date) => {
+  const year = date.getFullYear();
+
+  let month = date.getMonth() + 1;
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  let day = date.getDate();
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  return `${year}-${month}-${day}`
+}
+
+export default formatDate;
