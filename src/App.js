e7a04f7659364cf185d25ec5eec2c0d80834db10
Rocio <rociocelasco.r@gmail.com>
2022-10-05 12:38:40 +0200

    Agregar grafica

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import "./App.css";
 import { useState } from "react";
 import formatDate from "./formatDate";
+import Chart from "./Chart";
 
 const today = formatDate(new Date());
 
 function App() {
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
+  const [data, setData] = useState();
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     fetch(`https://apidatos.ree.es/es/datos/balance/balance-electrico?start_date=${startDate}T00:00&end_date=${endDate}T23:59&time_trunc=day`)
       .then((res) => res.json())
-      .then((data) => {
-        //console.log(data.included);
+      .then((json) => {
+        const attributes = json.included[0].attributes.content[1].attributes;
+        setData({
+          labels: attributes.values.map(item => item.datetime.split("T")[0]),
+          datasets: [
+            {
+              label: attributes.title,
+              data: attributes.values.map(item => item.value),
+              borderColor: attributes.color,
+              backgroundColor: attributes.color
+            },
+          ],
+        })
       });
   }
 
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
-        <label>Fecha de Inicio
+        <label><p>Fecha de Inicio</p>
           <input
             type="date"
             name="startDate"
@@ -29,7 +42,7 @@ function App() {
             onChange={(event) => setStartDate(event.target.value)} />
         </label>
 
-        <label>Fecha Final
+        <label><p>Fecha Final</p>
           <input
             type="date"
             name="endDate"
@@ -42,6 +55,7 @@ function App() {
           Generar Gráfica
         </button>
       </form>
+      <Chart data={data} />
     </div>
   );
 }
---
+++ src/Chart.js
@@ -0,0 +1,46 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const Chart = ({ data }) => {
+  if (!data) return;
+
+  return (
+    <div>
+      <Line options={options} data={data} />
+    </div>
+  )
+}
+
+export default Chart;
